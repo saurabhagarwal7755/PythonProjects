50225ca2084ad1336e91f584757d2e2f6cffa75f
Saurabh Kumar <52731148+saurabhagarwal7755@users.noreply.github.com>
2020-09-13 23:40:51 +0530

    Add files via upload

---
+++ Rock1.py
@@ -0,0 +1,35 @@
+#Rock, Paper, Scissors
+from random import randint      #for random numbers
+
+#Create a list of rock, paper and scissors
+items = ['rock', 'paper', 'scissor']
+
+#Assigning value to computer
+computer= items[randint(0,2)]
+
+#to get the player input value
+player = False
+
+while player == False:
+    player = input("rock,paper,scissor ? ")
+    if player == computer:
+        print("Tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print("You win!")
+        else:
+            print("You lose!")
+    elif player == "paper":
+        if computer == "rock":
+            print("You lose!")
+        elif computer == "scissors":
+            print("You win!")
+    elif player == "scissors":
+        if computer == "rock":
+            print("You win!")
+        else:
+            print("You win!")
+    else:
+        print("That\'s not a valid play.")
+    player = False      #make player false to continue the loop
+    computer= items[randint(0,2)]
