c304c6b4423f0a63665ff08980bd849d040f57eb
Saurabh Kumar <52731148+saurabhagarwal7755@users.noreply.github.com>
2020-12-11 22:33:16 +0530

    Add files via upload

---
+++ Passwordgenerator.py
@@ -0,0 +1,11 @@
+import string
+import random
+
+password_length = 10
+
+def password_generator(length):
+    password_characters = string.ascii_letters + string.digits + string.ascii_letters + string.punctuation
+    new_password = "".join(random.choice(password_characters) for i in range(password_length))
+    print(f"Your new password is {new_password}")
+
+password_generator(password_length)
